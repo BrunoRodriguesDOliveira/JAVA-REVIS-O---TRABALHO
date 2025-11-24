281bfe63be2a7bd13e6ff027795ff4a483fbd4b9
BrunoRodriguesDOliveira <brunor.dg@hotmail.com>
2025-11-24 17:09:28 -0300

    Add files via upload

---
+++ repeticoes.js
@@ -0,0 +1,21 @@
+// REPETIÇÕES
+
+//FOR
+
+//console.log("Contando até 5: ");
+//for(let i = 1; 1 <= 4; i++){
+    //console.log(i);
+//}
+
+//let contador = 3;
+//console.log("Contagem regressiva")
+//while(contador > 0){
+    //console.log(contador);
+    //contador--;
+//}
+
+let numero = 0;
+do{
+    console.log("Executando pelo menos uma vez");
+    numero++;
+}while(numero < 5);
